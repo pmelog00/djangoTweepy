e820954680769e19adca0318512f4ff3179204c5
Pablo Melón García <pmelog00@estudiantes.unileon.es>
2022-04-25 15:34:54 +0200

    Implementado el registro correctamente con la base de datos usando django crispy-forms y bootstrap. También implementadas las alerts con los messages de Django

--- env/djangoProject/djangoProject/settings.py
+++ env/djangoProject/djangoProject/settings.py
@@ -9,6 +9,19 @@ https://docs.djangoproject.com/en/4.0/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.0/ref/settings/
 """
+"""
+Añado CSS a las a los Django messages
+"""
+import os
+from django.contrib.messages import constants as messages
+
+MESSAGE_TAGS = {
+        messages.DEBUG: 'alert-secondary',
+        messages.INFO: 'alert-info',
+        messages.SUCCESS: 'alert-success',
+        messages.WARNING: 'alert-warning',
+        messages.ERROR: 'alert-danger',
+ }
 
 from pathlib import Path
 
@@ -37,7 +50,11 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'djangoApp.apps.DjangoappConfig',
+    'crispy_forms'
 ]
+
+CRISPY_TEMPLATE_PACK = 'bootstrap4'
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
@@ -72,7 +89,8 @@ WSGI_APPLICATION = 'djangoProject.wsgi.application'
 
 # Database
 # https://docs.djangoproject.com/en/4.0/ref/settings/#databases
-
+import pymysql
+pymysql.install_as_MySQLdb()
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.mysql',
