60d90e4442f92593facd0446b444cd5f35d43210
Pablo Melón García <pmelog00@estudiantes.unileon.es>
2022-04-27 10:47:23 +0200

    Implementado el login con crispy forms

--- env/djangoProject/djangoApp/urls.py
+++ env/djangoProject/djangoApp/urls.py
@@ -5,5 +5,6 @@ app_name = "djangoApp"
 
 urlpatterns = [
     path("", views.homepage, name="homepage"),
-    path("register", views.register_request, name="register")
+    path("register", views.register_request, name="register"),
+    path("login", views.login_request, name="login")
 ]
