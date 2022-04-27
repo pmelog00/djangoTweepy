72afadacb3dcbe4edc2f2ffd5cf075f4ef75106f
Pablo Melón García <pmelog00@estudiantes.unileon.es>
2022-04-27 17:02:45 +0200

    Implementado el logout, TODO: mejorar la navbar

--- env/djangoProject/djangoApp/urls.py
+++ env/djangoProject/djangoApp/urls.py
@@ -6,5 +6,6 @@ app_name = "djangoApp"
 urlpatterns = [
     path("", views.homepage, name="homepage"),
     path("register", views.register_request, name="register"),
-    path("login", views.login_request, name="login")
+    path("login", views.login_request, name="login"),
+    path("logout", views.logout_request, name="logout")
 ]
